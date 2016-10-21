1c681560d350bfb3f38196fde193dc5b3752ab93
lfawkes <lancefawkes@yahoo.com.au>
2016-10-21 11:56:03 +1000

    Add files via upload

---
+++ Lance-Fawkes-N9488316/Release-2/Volunteer_account_details.php
@@ -0,0 +1,86 @@
+<?php require 'Connections/connections.php';?>
+<?php
+session_start();
+//set variables
+$accountnumber = $_SESSION['ACCOUNTNUMBER'];
+$volunteer_account_number = $_SESSION['v_accountnumber'];
+$error_message = FALSE;
+$empty_field = FALSE;
+
+//check if form has been submitted
+if(isset($_POST['save'])){
+	//set local variables
+	$first_name = $_POST['f_name'];
+	$last_name = $_POST['l_name'];
+	$email = $_POST['email'];
+	$phonenumber = $_POST['phonenumber'];
+	
+	//Check if any field is empty. If not then create account.
+	if($_POST['f_name'] == "" || $_POST['l_name'] == "" || $_POST['email'] == "" || $_POST['phonenumber'] == ""){
+		$empty_field = TRUE;
+	} else {
+		$sql = "INSERT INTO volunteer (Account_number, FirstName, LastName, PhoneNumber, Email)
+			VALUES ('$volunteer_account_number', '$first_name', '$last_name', '$phonenumber', '$email')";
+		mysqli_query($con, $sql);
+		header ("Location: Account_created.php");
+	}
+} elseif(isset($_POST['cancel'])){
+	//Delete the newly created login account
+	$sql2 = "DELETE FROM account 
+			WHERE Account_number = '$volunteer_account_number'";
+	mysqli_query($con, $sql2);
+	header ("Location: login_registration.php");
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+<link href="css/Login.css" rel="stylesheet" type="text/css" />
+<meta charset="utf-8">
+<title>Edit Login</title>
+</head>
+
+<body>
+<div class="Login">
+  <h1 align="center">Account Details</h1>
+  <p align="center">Enter your account details below::</p>
+  <div>
+  <p align="center">
+  <?php 
+   if($empty_field == TRUE){
+		echo "Please fill in all details. You can edit it later"; 
+   }
+  ?>
+  </p>
+  </div>
+  <form action="" method="post" style="text-align:center;">
+  <p>
+    <label>Enter Firstname<br/>
+    </label>
+    <input name="f_name" type="text" autofocus id="f_name" placeholder="Enter here">
+    <br/>
+    <br/>
+    <label>Enter Lastname<br/>
+    </label>
+    <input name="l_name" type="text" autofocus id="l_name" placeholder="Enter here">
+  </p>
+  <p>
+    <label>Enter Email<br/>
+    </label>
+    <input name="email" type="email" autofocus id="email" placeholder="Enter here">
+    <br/>
+    </p> 
+    <p>
+    <label>Enter Phone Number<br/>
+    </label>
+    <input name="phonenumber" type="text" autofocus id="phonenumber" placeholder="Enter here">
+    <br/>
+    </p>   
+    <input name="save" type="submit" id="save" value="Confirm">	
+    <input name="cancel" type="submit" id="cancel" value="Cancel">
+  </p>
+  </form>
+</div>
+</body>
+</html>
